c109f4a276a036370ea50449b2dbf722cc284e96
Sanjar17 <sanjarsaidov173@gmail.com>
2023-04-15 11:00:16 +0500

    title is done

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -16,32 +16,73 @@ class _HomeScreenState extends State<HomeScreen> {
     return MaterialApp(
       theme: theme,
       home: Scaffold(
-        appBar: AppBar(
-          leading: Image.asset(image_103),
-          title: const Text(
-            '103',
-          ),
-        ),
-        body: const Center(child: Text('103')),
-        bottomNavigationBar: BottomAppBar(
-          shape: const CircularNotchedRectangle(),
-          child: Row(
-            // we're gonna change this  to active and inactive images
+        body: SafeArea(
+          child: Column(
             children: [
-              IconButton(icon: Image.asset(ambulance), onPressed: () {}),
-              IconButton(icon: Image.asset(profile), onPressed: () {}),
-              const Spacer(),
-              IconButton(icon: Image.asset(spravochnik), onPressed: () {}),
-              IconButton(icon: Image.asset(info), onPressed: () {}),
+              Container(
+                color: Colors.red,
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(50)),
+                              child: const Icon(Icons.phone_rounded,
+                                  color: Colors.red, size: 30),
+                            ),
+                            Text(
+                              appBarTitle,
+                              style: Theme.of(context).textTheme.titleLarge,
+                            ),
+                          ],
+                        ),
+                        // small title text
+                        const Text(appBarLeadingText),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              // change to map widget
+              const Center(child: Text('103')),
             ],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-            elevation: 2,
-            child: Image.asset(
-              image_103,
-            ),
-            onPressed: () {}),
+        bottomNavigationBar: BottomAppBar(
+          height: 60,
+          shape: const CircularNotchedRectangle(),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            // we're gonna change this  to active and inactive images
+            children: [
+              const Spacer(flex: 1),
+              IconButton(icon: Image.asset(ambulance), onPressed: () {}),
+              const Spacer(flex: 2),
+              IconButton(icon: Image.asset(profile), onPressed: () {}),
+              const Spacer(flex: 4),
+              IconButton(icon: Image.asset(spravochnik), onPressed: () {}),
+              const Spacer(flex: 2),
+              IconButton(icon: Image.asset(info), onPressed: () {}),
+              const Spacer(flex: 1),
+            ],
+          ),
+        ),
+        floatingActionButton: SizedBox(
+          width: 70,
+          height: 70,
+          child: FloatingActionButton(
+              elevation: 2,
+              child: Image.asset(
+                image_103,
+              ),
+              onPressed: () {}),
+        ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
     );
