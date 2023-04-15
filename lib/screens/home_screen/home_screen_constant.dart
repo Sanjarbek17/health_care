cf7b46899839f0dfb9ee7d67e92a55dbbf54dc2d
Sanjar17 <sanjarsaidov173@gmail.com>
2023-04-15 10:13:26 +0500

    some iamge added and inset added

--- lib/screens/home_screen/home_screen_constant.dart
+++ lib/screens/home_screen/home_screen_constant.dart
@@ -1,2 +1,18 @@
 // 103 image path
 String image_103 = 'assets/logo1034.png';
+// ambulance image path
+String ambulance = 'assets/map_grey.png';
+// ambulance active image path
+String ambulanceActive = 'assets/map_red.png';
+// profile image path
+String profile = 'assets/profil_grey.png';
+// profile active image path
+String profileActive = 'assets/profil_red.png';
+// spravochnik image path
+String spravochnik = 'assets/spravochnik_grey.png';
+// spravochnik active image path
+String spravochnikActive = 'assets/spravochnik_red.png';
+// info image path
+String info = 'assets/info_grey.png';
+// info active image path
+String infoActive = 'assets/info_red.png';
