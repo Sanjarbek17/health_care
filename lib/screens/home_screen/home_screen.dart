6aa24350bae565ee217b596d0f530c1aa743c342
Sanjar17 <sanjarsaidov173@gmail.com>
2023-04-15 11:06:27 +0500

    some changes

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -17,49 +17,47 @@ class _HomeScreenState extends State<HomeScreen> {
       theme: theme,
       home: Scaffold(
         body: SafeArea(
-          child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(10),
-                color: Colors.red,
-                child: Row(
+          child: Column(children: [
+            Container(
+              padding: const EdgeInsets.all(10),
+              color: Colors.red,
+              child: Row(children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            Container(
-                              padding: const EdgeInsets.all(10),
-                              decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(50)),
-                              child: const Icon(Icons.phone_rounded,
-                                  color: Colors.red, size: 30),
-                            ),
-                            Text(
-                              appBarTitle,
-                              style: Theme.of(context).textTheme.titleLarge,
-                            ),
-                          ],
+                    Row(children: [
+                      Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(50)),
+                        child: const Icon(
+                          Icons.phone_rounded,
+                          color: Colors.red,
+                          size: 30,
                         ),
-                        // small title text
-                        Text(
-                          appBarLeadingText,
-                          style: Theme.of(context).textTheme.titleSmall,
-                        ),
-                      ],
+                      ),
+                      Text(
+                        appBarTitle,
+                        style: Theme.of(context).textTheme.titleLarge,
+                      ),
+                    ]),
+                    // small title text
+                    Text(
+                      appBarLeadingText,
+                      style: Theme.of(context).textTheme.titleSmall,
                     ),
                   ],
                 ),
-              ),
-              // change to map widget
-              const Center(child: Text('103')),
-            ],
-          ),
+              ]),
+            ),
+            // change to map widget
+            const Center(child: Text('103')),
+          ]),
         ),
         bottomNavigationBar: BottomAppBar(
           height: 60,
+          elevation: 20,
           shape: const CircularNotchedRectangle(),
           child: Row(
             mainAxisSize: MainAxisSize.max,
