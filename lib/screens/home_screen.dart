98c0c229a2ed60d988c8ac494940530bdd4e3cb1
Sanjar17 <sanjarsaidov173@gmail.com>
2023-04-26 12:55:27 +0500

    main button extened added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 
 import '../screens/catalog_screen.dart';
 import '../screens/info_screen.dart';
@@ -6,6 +7,7 @@ import '../screens/map_screen.dart';
 import '../screens/profil_screen.dart';
 
 import '../style/main_style.dart';
+import '../style/my_flutter_app_icons.dart';
 
 import 'constants.dart';
 
@@ -26,6 +28,8 @@ class _HomeScreenState extends State<HomeScreen> {
   ];
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
     return MaterialApp(
       theme: theme,
       home: Scaffold(
@@ -78,12 +82,68 @@ class _HomeScreenState extends State<HomeScreen> {
         floatingActionButton: SizedBox(
           width: 70,
           height: 70,
-          child: FloatingActionButton(
-              elevation: 2,
-              child: Image.asset(
-                image_103,
+          child: SpeedDial(
+            icon: MyFlutterApp.logo1034,
+            activeIcon: Icons.close,
+            iconTheme: IconThemeData(color: Colors.red),
+            // this is ignored if animatedIcon is non null
+            // child: Icon(Icons.add),
+            visible: true,
+            // If true user is forced to close dial manually
+            // by tapping main button and overlay is not rendered.
+            // childPadding: EdgeInsets.all(30),
+            closeManually: false,
+            childrenButtonSize: Size(width * 0.9, 70),
+            curve: Curves.bounceIn,
+            overlayColor: Colors.black,
+            overlayOpacity: 0.5,
+            onOpen: () => print('OPENING DIAL'),
+            onClose: () => print('DIAL CLOSED'),
+            tooltip: '102',
+            heroTag: 'speed-dial-hero-tag',
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            elevation: 8.0,
+            shape: const CircleBorder(),
+            children: [
+              SpeedDialChild(
+                child: const ListTile(
+                  textColor: Colors.white,
+                  iconColor: Colors.white,
+                  leading: Icon(Icons.sms),
+                  title: Text(mainButtonThirdText),
+                  subtitle: Text(mainButtonThirdSubtitleText, style: TextStyle(fontSize: 10)),
+                ),
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(2)),
+                onTap: () => print('FIRST CHILD'),
               ),
-              onPressed: () {}),
+              SpeedDialChild(
+                child: const ListTile(
+                  textColor: Colors.white,
+                  iconColor: Colors.white,
+                  leading: Icon(Icons.phone_iphone_rounded),
+                  title: Text(mainButtonSecondText),
+                  subtitle: Text(mainButtonSecondSubtitleText, style: TextStyle(fontSize: 10)),
+                ),
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(2)),
+                onTap: () => print('FIRST CHILD'),
+              ),
+              SpeedDialChild(
+                child: const ListTile(
+                  textColor: Colors.white,
+                  iconColor: Colors.white,
+                  leading: Icon(Icons.place_outlined),
+                  title: Text(mainButtonFirstText),
+                  subtitle: Text(mainButtonFirstSubtitleText, style: TextStyle(fontSize: 10)),
+                ),
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(2)),
+                onTap: () => print('FIRST CHILD'),
+              ),
+            ],
+          ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
